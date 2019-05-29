57e304716d9fc6d368fd26fec9168834ed62204b
ERIC-BEAUSSANT-TORNE <eric.beaussant@ektok.se>
2019-05-29 08:54:04 +0200

    first

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>The Book Archive</title>
+    <link rel="stylesheet" href="./main.css">
+  </head>
+  <body>
+
+    <div class="wrapper">
+      <div class="head">
+        <h1>WIES18 bokverktyg</h1>
+      </div>
+      <div class="main-body">
+        <p id="amasing_text">Something amasing will happen with your CSV if you give me all your money.</p>
+        <br><br>
+        <p id="letsDoIt"><button type="button" name="LetsDoIt">Let's do it!</button></p>
+
+
+      </div>
+
+
+    </div>
+
+  </body>
+</html>
---
+++ uploads.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>The Book Archive</title>
+    <link rel="stylesheet" href="./main.css">
+  </head>
+  <body>
+
+    <div class="wrapper">
+      <div class="head">
+        <h1>WIES18 bokverktyg</h1>
+      </div>
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+          <p>Välj fil att ladda upp:</p>
+          <input type="file" name="fileToUpload" id="fileToUpload" value="Sök">
+          <input type="submit" value="Ladda upp" name="submit">
+      </form>
+
+      </div>
+
+
+    </div>
+
+  </body>
+</html>
