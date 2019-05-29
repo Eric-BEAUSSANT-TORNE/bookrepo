fa5eeb304ed5f4bffa83705574274b2b4a2f3290
ERIC-BEAUSSANT-TORNE <eric.beaussant@ektok.se>
2019-05-29 12:31:03 +0200

    third

---
+++ confandpay.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>The Book Archive</title>
+    <link rel="stylesheet" href="./main.css">
+  </head>
+  <body>
+
+    <div class="wrapper">
+      <div class="head">
+        <h1>WIES18 bokverktyg</h1>
+      </div>
+      <div class="left-body">
+        <p>Dina böcker:</p>
+        <?php
+        /*    session_start();
+
+            echo $_FILES['fileToUpload']['name'];
+            echo "<html><body><table>\n\n";
+            $f = fopen($_SESSION["upedfile"], "r");
+            while (($line = fgetcsv($f)) !== false) {
+                    echo "<tr>";
+                    foreach ($line as $cell) {
+                            echo "<td>" . htmlspecialchars($cell) . "</td>";
+                    }
+                    echo "</tr>\n";
+            }
+            fclose($f);
+            echo "\n</table></body></html>";*/
+         ?>
+      </div>
+      <div class="right-body">
+        <form class="" action="index.html" method="post">
+          <p>Fyll i din uppgifter:</p>
+          Namn: <input type="text" name="Namn" value="Sven Svensson"> <br>
+          Adress: <input type="text" name="Adress" value="Slottsbacken 1, 107 70 Stockholm"><br>
+          Telefonnummer: <input type="text" name="Telefon" value="0707070707"><br>
+          Kortuppgifter: <input type="text" name="Kort " value="1234 1234 1234 1234">
+          <input type="text" name="Cvv" value="123"><br>
+          <input type="checkbox" name="Godkänt" value=""> Jag godkänner vilkor och sånt. <br>
+          <input type="submit" name="Betalar" value="Kör!">
+
+        </form>
+      </div>
+
+    </div>
+
+  </body>
+</html>
--- uploads.php
+++ uploads.php
@@ -11,37 +11,58 @@
       <div class="head">
         <h1>WIES18 bokverktyg</h1>
       </div>
-      <form action="upload.php" method="post" enctype="multipart/form-data">
+      <form action="" method="POST" enctype="multipart/form-data">
           <p>Välj fil att ladda upp:</p>
-          <input type="file" name="fileToUpload" id="fileToUpload" value="Sök">
+          <input type="file" name="fileToUpload" id="fileToUpload" >
           <input type="submit" value="Ladda upp" name="submit">
       </form>
 
-      </div>
-<?php
-if (isset($_FILES)) {
-    $check = true;
-    if ($_FILES['fileToUpload']['type'] !== 'text/plain') {
-        $check = false;
-    }
+    </div>
+      <?php
+      session_start();
+      $phpFileUploadErrors = array(
+        0 => 'There is no error, the file uploaded with success',
+        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+        3 => 'The uploaded file was only partially uploaded',
+        4 => 'No file was uploaded',
+        6 => 'Missing a temporary folder',
+        7 => 'Failed to write file to disk.',
+        8 => 'A PHP extension stopped the file upload.',
+      );
+      if(isset($_FILES['fileToUpload'])){
 
-if ($check) {
-
-      $path = realpath('./') . '/UploadedFiles/' . $_FILES['fileToUpload']['name'];
-
-        if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "$path")) {
-            echo "Filen ". basename( $_FILES["fileToUpload"]["name"]). " har laddat up.";
-            ?>
-            <form action="user_data.php">
-                <input type="submit" value="Försätta>>">
-            </form>
-        <?php
-        } else {
-            echo "Tyvär det var nån fel me laddaup fil.";
+        $ext_error = false;
+        // Allowed extentions
+        $extenstions = array('csv');
+        // Check if uploaded file is allowed
+        $file_ext = end(explode('.',$_FILES['fileToUpload']['name']));
+        if($extentions == $file_ext){
+          $ext_error = true;
         }
-    }
-}
-?>
+        if($_FILES['fileToUpload']['error']){
+          echo $phpFileUploadErrors[$_FILES['fileToUpload']['error']];
+        }
+        elseif($ext_error){
+          echo 'Invalid file extention';
+        }
+        else
+        {
+          echo 'Uppladdning lyckad!';
+        }
+        //Saves file
+        move_uploaded_file($_FILES['fileToUpload']['tmp_name'],
+            './' . $_FILES['fileToUpload']['name']);
+
+            $_SESSION['upedfile'] = './' . $_FILES['fileToUpload']['name'];
+      }
+
+      function pre_r($array){
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+      }
+    ?>
 
   </body>
 </html>
